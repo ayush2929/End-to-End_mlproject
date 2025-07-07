29995da01a0ecba86d3ab3bb96125ece8a17b80b
Ayush <119004789+ayush2929@users.noreply.github.com>
2025-07-08 04:13:04 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,34 @@
-import os 
-import sys 
+import os
+import sys
 import dill
-
-
-import numpy as np 
-import pandas as pd 
-
-
-
+from sklearn.metrics import r2_score
 from src.exception import CustomException
+
 def save_object(file_path, obj):
+    """
+    Save a Python object to disk using dill.
+    """
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
 
+def evaluate_models(X_train, Y_train, X_test, Y_test, models, param=None):
+    """
+    Train and evaluate multiple models, returning a report of r2 scores.
+    """
+    try:
+        report = {}
+        for name, model in models.items():
+            model.fit(X_train, Y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(Y_train, y_train_pred)
+            test_model_score = r2_score(Y_test, y_test_pred)
+            report[name] = test_model_score
+        return report
     except Exception as e:
         raise CustomException(e, sys)
